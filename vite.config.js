8fd55c01e7e6b3d506215d971b5df81709bae39d
dianasauval <dianasauval@hotmail.com>
2025-04-25 00:28:14 -0300

    probando

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // 👈 Esto es clave
+    },
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,8 +17,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
-  build: {
-    outDir: 'dist',
   },
 })
