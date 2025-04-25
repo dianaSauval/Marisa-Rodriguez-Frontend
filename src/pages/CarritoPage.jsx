e45a29a4cccc405a93c2c36b1c53c55ba738a5b4
dianasauval <dianasauval@hotmail.com>
2025-04-25 00:44:51 -0300

    probando

--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -76,7 +76,7 @@ export default function CarritoPage() {
                     onClick={() => removerDelCarrito(curso.titulo)}
                   >
                     ✖
-                  </button>
+                  </button> 
                 </div>
               </li>
             ))}
